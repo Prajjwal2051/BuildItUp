9009cc939029fd4bf7571224c17065c25716a4c7
Prajjwal2051 <prajjwal2051@gmail.com>
2026-03-19 10:55:50 +0530

    feat: enhance authentication flow with user roles and route management

---
+++ proxy.ts
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth";
+import { 
+    DEFAULT_LOGIN_REDIRECT,
+    apiAuthPrefix,
+    publicRoutes,
+    authRoutes,
+
+ } from "./route";
+
+ import authConfig from "./auth.config";
+
+ const {auth}=NextAuth(authConfig)
+
+ export default auth((req)=>{
+    const {nextUrl} = req
+    const isLoggedIn = !!req.auth
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if(isApiAuthRoute){
+        return null
+    }
+
+    if(isAuthRoute){
+        if(isLoggedIn){
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,req.url))
+        }
+        return null
+    }
+    if(!isLoggedIn && !isPublicRoute){
+        return Response.redirect(new URL("/auth/signin",req.url))
+    }
+
+    return null
+    
+ })
+
+export const config = {
+    //clerk regex to match all routes except for static files and api routes
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
+  }
---
+++ route.ts
@@ -0,0 +1,29 @@
+// here we will be creating the routes for all other routes
+
+// this is the public routes which can be accessed by anyone without authentication
+export const publicRoutes: string[] = [
+    "/",
+]
+
+// this is the protected routes which can only be accessed by authenticated users
+export const protectedRoutes: string[] = [
+    "/dashboard",
+    "/profile",
+    "/settings",
+    "/admin",
+]
+
+// this is the auth routes which can only be accessed by unauthenticated users
+export const authRoutes: string[] = [
+    "/auth/signin",
+    "/auth/signup",
+]
+
+// an array of route which is accessible to the public
+
+export const apiAuthPrefix: string = "/api/auth"
+
+export const DEFAULT_LOGIN_REDIRECT: string = "/" // chnaged to redirect to home page after login
+
+
+
